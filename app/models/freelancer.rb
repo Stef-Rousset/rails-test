04204b43e4f9666d1fcf9a550557165b3108cb0e
stephanie rousset <strabon@club-internet.fr>
2021-01-06 11:39:56 +0100

    set flashes

--- app/models/freelancer.rb
+++ app/models/freelancer.rb
@@ -2,7 +2,7 @@ class Freelancer < ApplicationRecord
 
   validates :name, presence: true, length: { minimum: 2 }
   validates :email, presence: true, uniqueness: true
-  validates :phone_number, presence: true, length: { is: 10 }
+  validates :phone_number, presence: true
   validates :biography, presence: true, length: { in: 6..1000 }
 
 end
