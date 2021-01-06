bd80f0a59e67a6b0a4dc3febf5ad0a6987f9cf7d
stephanie rousset <strabon@club-internet.fr>
2021-01-06 11:30:36 +0100

    change type of phone number column in freelancer table

--- app/controllers/freelancers_controller.rb
+++ app/controllers/freelancers_controller.rb
@@ -2,11 +2,12 @@ class FreelancersController < ApplicationController
 
   def create
     @freelancer = Freelancer.new(freelancer_params)
-    if @freelancer.save
-      redirect_to root_path, notice: "Merci pour votre inscription, un email de confirmation vous a été envoyé"
+    if @freelancer.save!
+      flash[:notice] = "Merci pour votre inscription, un email de confirmation vous a été envoyé"
+      redirect_to root_path
     else
       flash.now[:alert] = "Votre inscription n'a pas été enregistrée"
-      render root_path
+      render :home
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_06_090635) do
+ActiveRecord::Schema.define(version: 2021_01_06_102843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2021_01_06_090635) do
   create_table "freelancers", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.integer "phone_number"
+    t.string "phone_number"
     t.text "biography"
     t.boolean "email_confirmed", default: false
     t.string "confirm_token"
