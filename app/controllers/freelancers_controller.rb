1f4c332c7546303391fdfb14e6c35f3fe10ebbe1
stephanie rousset <strabon@club-internet.fr>
2021-01-06 10:52:58 +0100

    improve style

--- app/controllers/freelancers_controller.rb
+++ app/controllers/freelancers_controller.rb
@@ -3,9 +3,10 @@ class FreelancersController < ApplicationController
   def create
     @freelancer = Freelancer.new(freelancer_params)
     if @freelancer.save
-      redirect_to root_path
+      redirect_to root_path, notice: "Merci pour votre inscription, un email de confirmation vous a été envoyé"
     else
-      render :new
+      flash.now[:alert] = "Votre inscription n'a pas été enregistrée"
+      render root_path
     end
   end
 
