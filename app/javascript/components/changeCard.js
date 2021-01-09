541b11502423560a64bafbd31a2d4cd23e90bb0a
stephanie rousset <strabon@club-internet.fr>
2021-01-09 18:09:38 +0100

    add comment to changeCard js file

--- app/javascript/components/changeCard.js
+++ app/javascript/components/changeCard.js
@@ -19,6 +19,7 @@ const changeCard = () => {
         card.style.backgroundImage = "url('/assets/internet-7169c063e29c2fee4826fd3cfb9432f333d6fdcb9f6d60e2faf98bfb56d2595e.png')";
       } else if (card.classList.contains('card-av-3')){
         card.style.backgroundImage = "url('./assets/worktogether-e30b9baffb85f6ec32786e371658d1da61186109bdb6458b87959f0b3d55ef4b.png')";
+        // ref de l'img recuperee dans la dev tools console
       };
     }));
   texts.forEach(text =>
