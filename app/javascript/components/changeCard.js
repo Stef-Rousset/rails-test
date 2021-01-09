baf3a1199e02beddbc0525b5f21fbdb8d6cac204
stephanie rousset <strabon@club-internet.fr>
2021-01-09 16:05:40 +0100

    add animations to cards

---
+++ app/javascript/components/changeCard.js
@@ -0,0 +1,30 @@
+const changeCard = () => {
+  const cards = document.querySelectorAll('.card-av');
+  const texts = document.querySelectorAll('.card-av-text');
+
+  cards.forEach(card =>
+    card.addEventListener('mouseover', function(){
+    card.style.backgroundImage ='none';
+    }));
+  texts.forEach(text =>
+    text.addEventListener('mouseover', function(){
+    text.classList.add('text-visible');
+    }));
+
+  cards.forEach(card =>
+    card.addEventListener('mouseout', function(){
+      if (card.classList.contains('card-av-1')){
+        card.style.backgroundImage = "url('assets/workstation.png')";
+      } else if (card.classList.contains('card-av-2')){
+        card.style.backgroundImage = "url('assets/internet.png')";
+      } else if (card.classList.contains('card-av-3')){
+        card.style.backgroundImage = "url('assets/worktogether.png')";
+      };
+    }));
+  texts.forEach(text =>
+    text.addEventListener('mouseout', function(){
+    text.classList.remove('text-visible');
+    }));
+};
+
+export { changeCard };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,8 +27,9 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
-
+import { changeCard } from '../components/changeCard';
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  changeCard();
 });
