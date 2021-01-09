0e859af5d9d5a505eac0bb51827a6e4a5d54b513
stephanie rousset <strabon@club-internet.fr>
2021-01-09 17:47:56 +0100

    try fix bug wth img card

--- app/javascript/components/changeCard.js
+++ app/javascript/components/changeCard.js
@@ -14,11 +14,11 @@ const changeCard = () => {
   cards.forEach(card =>
     card.addEventListener('mouseout', function(){
       if (card.classList.contains('card-av-1')){
-        card.style.backgroundImage = "url('./assets/workstation.png')";
+        card.style.backgroundImage = "url('/assets/workstation-4caf2d2ca3745cae381bfb19d2b0b0b6faf118aa5b2256edd4c2e46b6f7d7f39.png')";
       } else if (card.classList.contains('card-av-2')){
-        card.style.backgroundImage = "url('./assets/internet.png')";
+        card.style.backgroundImage = "url('/assets/internet-7169c063e29c2fee4826fd3cfb9432f333d6fdcb9f6d60e2faf98bfb56d2595e.png')";
       } else if (card.classList.contains('card-av-3')){
-        card.style.backgroundImage = "url('./assets/worktogether.png')";
+        card.style.backgroundImage = "url('./assets/worktogether-e30b9baffb85f6ec32786e371658d1da61186109bdb6458b87959f0b3d55ef4b.png')";
       };
     }));
   texts.forEach(text =>
